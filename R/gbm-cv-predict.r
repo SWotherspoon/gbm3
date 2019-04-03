24701fea0761ead0274c0aebb379a08c77adc257
Simon Wotherspoon <Simon.Wotherspoon@utas.edu.au>
2019-04-04 01:05:54 +1100

    CV updates
    Now return predictions for all observations from each fold.

--- R/gbm-cv-predict.r
+++ R/gbm-cv-predict.r
@@ -1,23 +1,23 @@
 #' Predictions for CV fitted GBM models
-#' 
+#'
 #' Calculate predictions from cross validated generalized boosting
 #' model from gbm2.
-#' 
+#'
 #' @param object a GBMCVFit object containing CV gbm models
-#' 
+#'
 #' @param gbm_data_obj a GBMData object containing all of the data
 #' used to fit a gbm model.
-#' 
+#'
 #' @param cv_folds a positive integer specifying the number of folds
 #' to be used in cross-validation of the gbm fit.
-#' 
+#'
 #' @param cv_group vector of integers specifying which row of data
 #' belongs to which cv_fold.
-#' 
+#'
 #' @param best_iter_cv number of trees with the smallest cv error
 #'
 #' @param \dots not currently used
-#' 
+#'
 #' @author James Hickey
 #'
 #' @return a matrix of predictions for each cv fold.
@@ -28,25 +28,23 @@ predict.GBMCVFit <- function(object, gbm_data_obj, cv_folds,
 
   # Extract data
   data <- gbm_data_obj$original_data[seq_len(object[[1]]$params$num_train), ,drop=FALSE]
-  
+
   ## test cv_group and data match
   if (nrow(data) != length(cv_group)) {
     stop("mismatch between data and cv_group")
   }
-  
+
   #Flatten data for prediction
-  num_cols <- 1
-  result <- matrix(nrow=nrow(data), ncol=num_cols)
-  data_names <- names(data)
-  
+  result <- matrix(nrow=nrow(data), ncol=cv_folds+1)
+
   for (ind in seq_len(cv_folds)) {
     flag <- cv_group == ind
     model <- object[[ind+1]]
-    my_data  <- data[flag, model$variables$var_names, drop=FALSE]
+    my_data  <- data[, model$variables$var_names, drop=FALSE]
     predictions <- predict(model, newdata=my_data, n.trees=best_iter_cv)
-    predictions <- matrix(predictions, ncol=num_cols)
-    result[flag,] <- predictions
+    result[,ind+1] <- predictions
+    result[flag,1] <- predictions[flag]
   }
-  
-  return(as.numeric(result))
+
+  return(result)
 }
